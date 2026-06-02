a128a99e865ba933fc034e1704c1243a415d6af3
EstefanoDiaz <145041876+EstefanoDiaz@users.noreply.github.com>
2026-06-01 22:30:14 -0500

    Seguridad Rutas - Seeder User

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\MarcaController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\admin\VehicleColorController;
@@ -10,17 +9,14 @@ use App\Http\Controllers\admin\BrandController;
 use App\Http\Controllers\admin\UserTypeController;
 use App\Http\Controllers\admin\UserController;
 use App\Http\Controllers\admin\VehicleController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
 */
 
+// Redirección inicial pública
 Route::redirect('/', '/login');
 
 Route::middleware([
@@ -28,17 +24,27 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
+
+    // Ruta base del Dashboard de Jetstream
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
-});
 
-Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
-Route::resource('color', VehicleColorController::class)->names('admin.color');
-Route::resource('brandmodel', BrandModelController::class)->names('admin.brandmodel');
-Route::resource('tipo-vehiculo', VehicleTypeController::class)->names('admin.tipo-vehiculo');
-Route::resource('brand', BrandController::class)->names('admin.brand');
-Route::resource('user-type', UserTypeController::class)->names('admin.usertype');
-Route::resource('user', UserController::class)->names('admin.user');
-Route::get('vehicle/models-by-brand', [VehicleController::class, 'modelsByBrand'])->name('admin.vehicle.modelsByBrand');
-Route::resource('vehicle', VehicleController::class)->names('admin.vehicle');
+    // MÓDULO ADMINISTRATIVO (PROTEGIDO)
+    Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
+    
+    // CRUDs de Configuración y Tablas Maestras
+    Route::resource('color', VehicleColorController::class)->names('admin.color');
+    Route::resource('brandmodel', BrandModelController::class)->names('admin.brandmodel');
+    Route::resource('tipo-vehiculo', VehicleTypeController::class)->names('admin.tipo-vehiculo');
+    Route::resource('brand', BrandController::class)->names('admin.brand');
+    Route::resource('user-type', UserTypeController::class)->names('admin.usertype');
+    
+    // CRUD de Personal / Usuarios (El que acabamos de armar con estado y foto)
+    Route::resource('user', UserController::class)->names('admin.user');
+    
+    // Módulo de Vehículos y su ruta auxiliar AJAX para los modelos
+    Route::get('vehicle/models-by-brand', [VehicleController::class, 'modelsByBrand'])->name('admin.vehicle.modelsByBrand');
+    Route::resource('vehicle', VehicleController::class)->names('admin.vehicle');
+
+});
